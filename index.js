fef314faec47432a90d96c147a2c26b24e08cbe8
narcis <narcissprecic@gmail.com>
2021-12-10 16:27:47 -0500

    Added forgotten task

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const data = require("./data.js");
+var _ = require('lodash');
 
 console.log(data.numbers);
 console.log(data.name + " is from " +data.hometown)
 
 const sum = require("./sum.js");
 
-console.log(sum.sumOfArray(data.numbers)); 
+console.log(sum.sumOfArray(data.numbers));
+console.log(_.shuffle(data.numbers)); //shuffles the array
